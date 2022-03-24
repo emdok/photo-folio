6d48b8df67c4564733fe04c0e736848082083ec8
Emily Dokken <dokkenemily@gmail.com>
2022-03-23 20:09:21 -0700

    add form handlers for form submission

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -23,6 +25,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
